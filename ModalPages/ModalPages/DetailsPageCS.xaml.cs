56507b5cacd061aa0be2ff0ece47cc543efacac4
Jasper Orilla <jasper.orilla915@gmail.com>
2019-08-05 00:30:01 +0800

    Add project files.

---
+++ ModalPages/ModalPages/DetailsPageCS.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ModalPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsPageCS : ContentPage
+    {
+        public DetailsPageCS()
+        {
+            var nameLabel = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                FontAttributes = FontAttributes.Bold
+            };
+            nameLabel.SetBinding(Label.TextProperty, "Name");
+
+            var ageLabel = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                FontAttributes = FontAttributes.Bold
+            };
+            ageLabel.SetBinding(Label.TextProperty, "Age");
+
+            var occupationLabel = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                FontAttributes = FontAttributes.Bold
+            };
+            occupationLabel.SetBinding(Label.TextProperty, "Occupation");
+
+            var countryLabel = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                FontAttributes = FontAttributes.Bold
+            };
+            countryLabel.SetBinding(Label.TextProperty, "Country");
+
+            var dismissButton = new Button { Text = "Dismiss" };
+            dismissButton.Clicked += OnDismissButtonClicked;
+
+
+            Content = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    new StackLayout {
+                        Orientation = StackOrientation.Horizontal,
+                        Children = {
+                            new Label {
+                                Text = "Name:",
+                                FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                                HorizontalOptions = LayoutOptions.FillAndExpand
+                            },
+                            nameLabel
+                        }
+                    },
+                    new StackLayout {
+                        Orientation = StackOrientation.Horizontal,
+                        Children = {
+                            new Label {
+                                Text = "Age:",
+                                FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                                HorizontalOptions = LayoutOptions.FillAndExpand
+                            },
+                            ageLabel
+                        }
+                    },
+                    new StackLayout {
+                        Orientation = StackOrientation.Horizontal,
+                        Children = {
+                            new Label {
+                                Text = "Occupation:",
+                                FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                                HorizontalOptions = LayoutOptions.FillAndExpand
+                            },
+                            occupationLabel
+                        }
+                    },
+                    new StackLayout {
+                        Orientation = StackOrientation.Horizontal,
+                        Children = {
+                            new Label {
+                                Text = "Country:",
+                                FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                                HorizontalOptions = LayoutOptions.FillAndExpand
+                            },
+                            countryLabel
+                        }
+                    },
+                    dismissButton
+                }
+            };
+
+        }
+        async void OnDismissButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
---
+++ ModalPages/ModalPages/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using ModalPages.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ModalPages
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        List<Contacts> contact;
+        public MainPage()
+        {
+            InitializeComponent();
+            SetUpdata();
+            Listview.ItemsSource = contact;
+        }
+        async void Listview_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (Listview.ItemsSource != null)
+            {
+                var detailpage = new DetailsPageCS();
+                detailpage.BindingContext = e.SelectedItem as Contacts;
+                Listview.SelectedItem = null;
+                await Navigation.PushAsync(detailpage);
+            }
+        }
+        void SetUpdata()
+        {
+            contact = new List<Contacts>();
+            contact.Add(new Contacts
+            {
+                Name = "Logesh",
+                Age = 20,
+                Occupation = "Farmer",
+                Country = "India"
+            });
+            contact.Add(new Contacts
+            {
+                Name = "Hari",
+                Age = 20,
+                Occupation = "Software Engineer",
+                Country = "India"
+            });
+            contact.Add(new Contacts
+            {
+                Name = "Mani",
+                Age = 20,
+                Occupation = "Farmer",
+                Country = "India"
+            });
+            contact.Add(new Contacts
+            {
+                Name = "Guna Seker",
+                Age = 20,
+                Occupation = "Farmer",
+                Country = "India"
+            });
+            contact.Add(new Contacts
+            {
+                Name = "Palani",
+                Age = 20,
+                Occupation = "Farmer",
+                Country = "India"
+            });
+            contact.Add(new Contacts
+            {
+                Name = "Laxman",
+                Age = 30,
+                Occupation = "Driver",
+                Country = "India"
+            });
+        }
+    }
+}
